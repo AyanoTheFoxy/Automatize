71dba3fb88a4ec706f0ca8b0a066c0efcc8939c8
Foxy <122460205+AyanoTheFoxy@users.noreply.github.com>
2023-01-11 14:45:45 -0300

    Update Math.py

--- Math.py
+++ Math.py
@@ -1,10 +1,7 @@
-
-#######################################################################
-a = int(input('Escreva valor de a '))
-b = int(input('Escreva valor de b '))
-c = int(input('Escreva valor de c '))
-
-delta = b**2 - 4 * a * c
+# crated by:Foxy
+# for: help with math
+#########################################################################
+delta  =  b ** 2  -  4  *  a  *  c
 x1 = (-b + delta**(1/2)) / (2 * a)
 x2 = (-b - delta**(1/2)) / (2 * a)
 
@@ -15,8 +12,8 @@ print('x2 = ',x2)
 print('Se a conta saiu etranha e por que os valores tao errados tente novamente')
 #######################################################################
 
-cat1 = float(input('Escreva o cateto adjacente '))
-cat2 = float(input('Escreva o cateto oposto '))
+cat1  =  float ( input ( 'Escreva o cateto adjacente ' ))
+cat2  =  float ( input ( 'Escreva o cateto oposto ' ))
 
 hpt = cat1**2 + cat2**2
 ptint('O quadrado da hipotenuza vale: ', hpt)
